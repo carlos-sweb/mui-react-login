2dc3539dd145283d53cf5d7f00f2ddbbc51fd76e
carlos-sweb <c4rl0sill3sc4@gmail.com>
2024-12-11 12:44:48 -0300

    remove input server

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,28 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CircularProgress from '@mui/material/CircularProgress';
 
+import axios from "axios"
+
 function App() {
-  
+  const url_auth = "http://preciointeligente.ddns.net:8000/api-token-auth/";
   const [username,setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [server,setServer] = useState("");
+  const [password, setPassword] = useState("")  
   const [sending,setSending] = useState(false);  
   const valid = ()=>{
-    return username != "" && password != "" && server != "";
+    return username != "" && password != "";
   }  
 
   const login = function(){
       console.log(username);
       setSending(true);
-      setTimeout(()=>{
-          setSending(false);
-      },3000);
+      
+      axios.post( url_auth , {
+        username: username , 
+        password: password
+      } ).then(( response )=>{
+          console.log( response );
+      });
+
   }
 
   return (    
@@ -34,8 +40,7 @@ function App() {
         <h1 className="text-3xl font-bold text-gray-900">Acceso</h1>
         <Box component="form" noValidate autoComplete="off" className="pt-3 flex flex-col gap-4 justify-center items-center" >
           <TextField type="text" fullWidth disabled={ sending } id="user" label="Usuario" variant="standard" value={username} onChange={e => setUsername(e.target.value)}   />
-          <TextField type="password" fullWidth disabled={ sending } id="pass" label="Contraseña" variant="standard" value={password} onChange={e => setPassword(e.target.value) } />
-          <TextField type="text" fullWidth disabled={ sending } id="server" label="Servidor" variant="standard" value={server} onChange={e => setServer(e.target.value) } />
+          <TextField type="password" fullWidth disabled={ sending } id="pass" label="Contraseña" variant="standard" value={password} onChange={e => setPassword(e.target.value) } />          
           { !sending  && <Button fullWidth disabled={ !valid() }  variant="contained" onClick={login} >Acceder</Button>}          
           { sending &&  <CircularProgress size={28} /> }
         </Box>                              
